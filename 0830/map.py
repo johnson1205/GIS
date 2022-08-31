0d6964b6cf00d28cb12c8743ad97775b3d20287a
Black_block <46107558+johnson1205@users.noreply.github.com>
2022-08-31 13:18:25 +0800

    Add files via upload

--- 0830/map.py
+++ 0830/map.py
@@ -1,30 +1,8 @@
 from lib2to3.pytree import convert
-import bs4, requests, os, json, folium
+import bs4, requests, os, json, folium, tqdm
 import concurrent.futures
-'''
-headers = { 'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64)\
-            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101\
-            Safari/537.36', }
-url = 'https://ptx.transportdata.tw/MOTC/v2/Bus/Stop/City/NewTaipei?%24top=21000&%24format=JSON'
-html = requests.get(url, headers=headers)
-objSoup = bs4.BeautifulSoup(html.text, 'lxml')
-j= json.loads(objSoup.text)
-m = folium.Map([25.046436, 121.517463], tiles="Cartodb Positron", zoom_start=14)
-for i in j:
-    longtitude = i['StopPosition']['PositionLon']
-    latitude = i['StopPosition']['PositionLat']
-    stopname = i['StopName']['Zh_tw']
-    folium.CircleMarker(
-        location=[latitude, longtitude],
-        radius=3,
-        popup=stopname,
-        color='#3186cc',
-        fill=True,
-        fill_color='#3186cc'
-    ).add_to(m)
 
-m.save('myMap.html')
-'''
+
 
 jsfileNewTaipei = open("response_1661839141867.json", 'r', encoding='UTF-8')
 jswordNewTaipei = jsfileNewTaipei.read()
@@ -43,17 +21,51 @@ for i in j2:
         j.append(i)
         geohash.append(i['StopPosition']['GeoHash'])
 
-m = folium.Map([25.046436, 121.517463], tiles="Cartodb Positron", zoom_start=14)
+m = folium.Map([25.046436, 121.517463], tiles="Cartodb Positron", zoom_start=11, control_scale=True)
 outfile = open("stationlist.txt", "w", encoding='UTF-8')
+
+#---
+def popup_html(i):
+    nameMandarin = i['StopName']['Zh_tw']
+    nameEnglish = ""
+    try:
+        nameEnglish=i['StopName']['En']
+    except:
+        nameEnglish=""
+    html = """
+<!DOCTYPE html>
+<html>
+<table style="border-collapse: collapse; width: 300px; height: 36px;" border="1">
+<tbody>
+<tr style="height: 18px;">
+<td style="text-align: center;background-color: """+ "#3366ff" +""";"colspan="2" style="width: 150px; height: 18px;">
+<h4><span style="color: #ffffff;"><strong>{}</strong></span></h4>""".format(nameMandarin)+"""
+<p><span style="color: #ffffff;"><strong>{}</strong></span></p>""".format(nameEnglish)+"""
+</td>
+</tr>
+<tr style="height: 18px;">
+<td style="width: 100px; height: 18px;">
+<p><strong>行經路線<p>
+</td>
+<td style="width: 200px; height: 18px;">&nbsp;</td>
+</tr>
+</tbody>
+</table>
+<p>&nbsp;</p>
+</html>
+"""
+    return html
+#---
+
 for i in j:
     longtitude = i['StopPosition']['PositionLon']
     latitude = i['StopPosition']['PositionLat']
     stopname = i['StopName']['Zh_tw']
     outfile.write(stopname+' '+str(latitude)+str(longtitude)+'\n')
-    folium.CircleMarker(
+    folium.Circle(
         location=[latitude, longtitude],
         radius=3,
-        popup=stopname,
+        popup=folium.Popup(folium.Html(popup_html(i), script=True),parse_html=False, max_width=300),
         color='#3186cc',
         fill=True,
         fill_color='#3186cc'
